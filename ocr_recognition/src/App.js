336dd9a4757b077b940bc42f86e5c712d37fb92a
aaryan2301 <20uec035@lnmiit.ac.in>
2023-12-25 20:50:35 +0530

    final commit

--- ocr_recognition/src/App.js
+++ ocr_recognition/src/App.js
@@ -7,37 +7,44 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState('');
   const [ocrRecords, setOcrRecords] = useState([]);
   const [showRecords, setShowRecords] = useState(false);
+  const [filterCriteria, setFilterCriteria] = useState('');
 
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
   };
 
   const extractInfo = (thaiText) => {
-    const lines = thaiText.split('\n');
-
-  const identificationIndex = lines.findIndex(line => line.includes('Identification Number'));
-  const nameIndex = lines.findIndex(line => line.includes('Name'));
-  const lnameIndex = lines.findIndex(line => line.includes('Last name'));
-  const dobIndex = lines.findIndex(line => line.includes('Date of Birth'));
-  const issueDateIndex = lines.findIndex(line => line.includes('Date of Issue'));
-  const expiryDateIndex = lines.findIndex(line => line.includes('Date of Expiry'));
-
-  const identificationNumber = lines[identificationIndex - 2].trim();
-  const name = lines[nameIndex].split('Name')[1].trim();
-  const lname = lines[lnameIndex].split('Last name')[1].trim();
-  const dateOfBirth = lines[dobIndex].split('Date of Birth')[1].trim();
-  const dateOfIssue = lines[issueDateIndex-1].trim();
-  const dateOfExpiry = lines[expiryDateIndex-1].trim();
-
-  return {
-    identification_number: identificationNumber,
-    name,
-    last_name: lname, // Assuming last name is the third part of the name
-    date_of_birth: dateOfBirth,
-    date_of_issue: dateOfIssue,
-    date_of_expiry: dateOfExpiry,
+    try {
+      const lines = thaiText.split('\n');
+  
+      const identificationIndex = lines.findIndex(line => line.includes('Identification Number'));
+      const nameIndex = lines.findIndex(line => line.includes('Name'));
+      const lnameIndex = lines.findIndex(line => line.includes('Last name'));
+      const dobIndex = lines.findIndex(line => line.includes('Date of Birth'));
+      const issueDateIndex = lines.findIndex(line => line.includes('Date of Issue'));
+      const expiryDateIndex = lines.findIndex(line => line.includes('Date of Expiry'));
+  
+      const identificationNumber = lines[identificationIndex - 2].trim();
+      const name = lines[nameIndex].split('Name')[1].trim();
+      const lname = lines[lnameIndex].split('Last name')[1].trim();
+      const dateOfBirth = lines[dobIndex].split('Date of Birth')[1].trim();
+      const dateOfIssue = lines[issueDateIndex - 1].trim();
+      const dateOfExpiry = lines[expiryDateIndex - 1].trim();
+  
+      return {
+        identification_number: identificationNumber,
+        name,
+        last_name: lname, // Assuming last name is the third part of the name
+        date_of_birth: dateOfBirth,
+        date_of_issue: dateOfIssue,
+        date_of_expiry: dateOfExpiry,
+      };
+    } catch (error) {
+      console.error("Error occurred:", error.message);
+      return {}; // Return an empty object in case of an error
+    }
   };
-  };
+  
 
   const createRecord = async () => {
     if (ocrData && ocrData.text) {
@@ -61,7 +68,17 @@ const App = () => {
     }
   };
 
-
+  const deleteRecord = async (customId) => {
+    try {
+      const response = await axios.delete(`http://localhost:5001/ocr/delete/${customId}`);
+      console.log('Record deleted:', response.data);
+      // After successfully deleting the record, you might want to update the UI or state
+      // For example, you can fetch the updated records after deletion and update the state
+      handleDisplay(); // Assuming handleDisplay fetches updated records
+    } catch (error) {
+      console.error('Error deleting record:', error);
+    }
+  };
   const displayRecords = () => {
     if(showRecords){
       return (
@@ -75,6 +92,7 @@ const App = () => {
                 <strong>Timestamp:</strong> {record.timestamp} <br />
                 <strong>Status:</strong> {record.status} <br />
                 <strong>ID:</strong> {record.customId} <br />
+                <button onClick={() => deleteRecord(record.customId)}>Delete</button>
                 <hr />
               </li>
             ))}
@@ -85,10 +103,33 @@ const App = () => {
     return null;
   };
   
+  const handleFilter = async () => {
+    try {
+      console.log(filterCriteria);
+      const response = await axios.post(`http://localhost:5001/ocr/filter/${filterCriteria}`);
+      setOcrRecords(response.data); // Update the state with filtered records
+      setShowRecords(true); // Set showRecords to true to display filtered records
+    } catch (error) {
+      console.error('Error filtering records:', error);
+    }
+  };
 
   const handleFileUpload = async () => {
     if (!selectedFile) {
       setErrorMessage('Please select a file.');
+      return;
+    }
+
+    const fileSizeInMB = selectedFile.size / (1024 * 1024);
+    if (fileSizeInMB > 2) {
+      setErrorMessage('File size exceeds the limit (2MB). Please select a smaller file.');
+      return;
+    }
+  
+    // Check file format
+    const allowedFormats = ['image/jpeg', 'image/png', 'image/jpg'];
+    if (!allowedFormats.includes(selectedFile.type)) {
+      setErrorMessage('Invalid file format. Please select a file in JPG, PNG, or JPEG format.');
       return;
     }
 
@@ -108,7 +149,7 @@ const App = () => {
     try {
       const response = await axios.request(options);
       setOcrData(response.data);
-      console.log(response.data);
+      console.log(response.data.text);
 
       if (ocrData && ocrData.text) {
         const extractedInfo = extractInfo(ocrData.text);
@@ -137,6 +178,19 @@ const App = () => {
       <input type="file" accept=".png, .jpg, .jpeg" onChange={handleFileChange} />
       <button onClick={handleFileUpload}>Upload</button>
       <button onClick={handleDisplay}>Display</button>
+      {showRecords && (
+        <div>
+          <select onChange={(e) => setFilterCriteria(e.target.value)}>
+            <option value="">Select Filter</option>
+            <option value="name">Name</option>
+            <option value="date_of_issue">Date of Issue</option>
+            <option value="date_of_expiry">Date of Expiry</option>
+            <option value="status">Successful</option>
+          </select>
+          <button onClick={handleFilter}>Filter</button>
+        </div>
+      )}
+      
       {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
       {ocrData && (
         <div>
